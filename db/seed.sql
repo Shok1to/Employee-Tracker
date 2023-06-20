8381a63312540e8f3d4f89371f4d6c13da11a42a
Shok1to <shoko.takahashi.911@gmail.com>
2023-06-20 17:22:11 -0400

    added seed sql

--- db/seed.sql
+++ db/seed.sql
@@ -13,7 +13,18 @@ VALUES
     ('Salesperson', 80000.00, 1),
     ('Lead Engineer', 150000.00, 2),
     ('Software Engineer', 120000.00, 2),
-    ('Accountant Manager', 160000.00, 3),
+    ('Account Manager', 160000.00, 3),
     ('Accountant', 125000.00, 3),
     ('Legal Team Lead', 250000.00, 4),
-    ('Lawyer', 190000.00, 4),
+    ('Lawyer', 190000.00, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1), 
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
