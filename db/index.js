6801bfc1a7ff3128c075a54a7f2431cf96784a3e
Shok1to <shoko.takahashi.911@gmail.com>
2023-06-28 21:04:22 -0400

    added an employee db

--- db/index.js
+++ db/index.js
@@ -1,25 +1,26 @@
-const connection = require('./connection')
-class DB{
-    constructor(connection){
-        this.connection = connection
-  
+const connection = require('./connection');
+
+class DB {
+    constructor(connection) {
+        this.connection = connection;
     }
-    viewAllEmployees(){
+    viewAllEmployees() {
         return this.connection.promise().query(`SELECT employee.id, employee.first_name,employee.last_name,role.title,department.name,role.salary FROM employee LEFT JOIN role on employee.role_id=role.id LEFT JOIN department on role.department_id=department.id;`)
     }
-    viewAllRoles(){
+    viewAllRoles() {
         return this.connection.promise().query(`SELECT * FROM role`)
     }
-    viewAllDepartments(){
+    viewAllDepartments() {
         return this.connection.promise().query(`SELECT * FROM department`)
     }
-    addDepartment(){
-        return this.connection.promise().query(`INSERT INTO department SET?`)
+    addDepartment(department) {
+        return this.connection.promise().query(`INSERT INTO department SET name = ?`, department)
     }
-    // addEmployee(employee){
-    //     return this.connection.promise().query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('?', ?, ?, ?), [employee.first_name, employee.last_name, employee.role_id, employee.manager_id]`);
-    // }
+    addEmployee(employee){
+        // return this.connection.promise().query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('?', ?, ?, ?), [employee.first_name, employee.last_name, employee.role_id, employee.manager_id]`);
+        return this.connection.promise().query(`INSERT INTO employee SET ?`, employee);
+    }
 
 }
-module.exports = new DB(connection)
+module.exports = new DB(connection);
 
--- index.js
+++ index.js
@@ -2,15 +2,14 @@
 const inquirer = require('inquirer');
 const DB = require('./db')
 
-function init(){
+function init() {
 
-
-inquirer
-  .prompt({
-    type: 'list',
-    name: 'category',
-    message: 'What would you like to do?',
-    choices: [
+  inquirer
+    .prompt({
+      type: 'list',
+      name: 'category',
+      message: 'What would you like to do?',
+      choices: [
         'View all employees',
         'Add an employee',
         'Update employee role',
@@ -19,137 +18,161 @@ inquirer
         'View all departments',
         'Add department',
         'Exit'
-    ]
+      ]
 
-})
-  .then((answers) => {
-    console.log(answers.category)
-    if(answers.category==='View all employees'){
+    }).then((answers) => {
+      console.log(answers.category)
+      if (answers.category === 'View all employees') {
         DB.viewAllEmployees()
-        .then(([data])=>{
+          .then(([data]) => {
             console.table(data)
             init()
-        })
-  }})
-    if(answers.category==='View all roles'){
-         DB.viewAllRoles()
-        .then(([data])=>{
+          })
+      }
+      if (answers.category === 'View all roles') {
+        DB.viewAllRoles()
+          .then(([data]) => {
             console.table(data)
             init()
-        })
-    }
-    if(answers.category==='View all departments'){
+          })
+      }
+      if (answers.category === 'View all departments') {
         DB.viewAllDepartments()
-        .then(([data])=>{
+          .then(([data]) => {
             console.table(data)
             init()
-        })
-    }
-    
-    if(answers.category==='Add Department'){
-       inquirer
-       .prompt([
-            { 
-              name: "department_name",
-              type: "input",
-              message: "What is the name of the department?",
-            },
+          })
+      }
+      if (answers.category === 'Add department') {
+        createDept();
+
+      }
+      if (answers.category === 'Add an employee') {
+        createEmployee()
+      }
+    })
 
 
-          ])
-            .then((answer) => {
-                DB.addDepartment(answer) 
-            //   console.log(DB.addDepartment(answer).department_name); 
-            }); 
-            
-        
-        
-    // }
-    // if(answers.category==='Add Role'){
-    //    inquirer
-    //    .prompt([
-    //         { 
-    //           name: "role_name",
-    //           type: "input",
-    //           message: "What is the name of the role",
-    //         },
-    //         { 
-    //           name: "salary",
-    //           type: "input",
-    //           message: "What is the salary of the role?",
-    //         },
-    //         { 
-    //             name: "department_id",
-    //             type: "list",
-    //             message: "Which department does the role belong to?",
-    //             choices: [
-    //             { name: 'Sales', value: 1 },
-    //             { name: 'Engineer', value: 2 },
-    //             { name: 'Finance', value: 3 },
-    //             { name: 'Legal', value: 4 },
-    //             
 
-    //             ]
-    //         },
-    //       ])
-    //         .then((answer) => {
-    //             DB.addEmployee(answer) 
-    //         //   console.log(DB.addEmployee(answer).first_name, answer.last_name,role_id,manager_id); 
-    //         }); 
-            
-        
-        
-    // }
-    // if(answers.category==='Add an employee'){
-        //    inquirer
-        //    .prompt([
-        //         { 
-        //           name: "first_name",
-        //           type: "input",
-        //           message: "What is the employee's first name?",
-        //         },
-        //         { 
-        //           name: "last_name",
-        //           type: "input",
-        //           message: "What is the employee's last name?",
-        //         },
-        //         { 
-        //             name: "role_id",
-        //             type: "list",
-        //             message: "What is the employee's role?",
-        //             choices: [{ name: 'Sales Lead', value: 1 },
-        //             { name: 'Salesperson', value: 2 },
-        //             { name: 'Lead Engineer', value: 3 },
-        //             { name: 'Software Engineer', value: 4 },
-        //             { name: 'Account Manager', value: 5 },
-        //             { name: 'Accountant', value: 6 },
-        //             { name: 'Legal Team Lead', value: 7 },
-        //             { name: 'Lawyer', value: 8 }
-    
-        //             ]
-        //         },
-        //         { 
-        //             name: "manager_id",
-        //             type: "list",
-        //             message: "Who is the employee's manager?",
-        //             choices: [{ name: 'John Doe', value: 1 },
-        //             { name: 'Mike Chan', value: 2 },
-        //             { name: 'Ashley Rodriguez', value: 3 },
-        //             { name: 'Kevin Tupik', value: 4 },
-        //             { name: 'Kunal Singh', value: 5 },
-        //             { name: 'Malia Brown', value: 6 },
-        //             { name: 'Sarah Lourd', value: 7 },
-        //             { name: 'Tom Allen', value: 8 }
-                       
-    
-        //             ]
-        //         },
-    
-        //       ])
-        //         .then((answer) => {
-        //             DB.addEmployee(answer) 
-        //         //   console.log(DB.addEmployee(answer).first_name, answer.last_name,role_id,manager_id); 
-    
-  }
+
+
+
+
+  // if(answers.category==='Add Role'){
+  //    inquirer
+  //    .prompt([
+  //         { 
+  //           name: "role_name",
+  //           type: "input",
+  //           message: "What is the name of the role",
+  //         },
+  //         { 
+  //           name: "salary",
+  //           type: "input",
+  //           message: "What is the salary of the role?",
+  //         },
+  //         { 
+  //             name: "department_id",
+  //             type: "list",
+  //             message: "Which department does the role belong to?",
+  //             choices: [
+  //             { name: 'Sales', value: 1 },
+  //             { name: 'Engineer', value: 2 },
+  //             { name: 'Finance', value: 3 },
+  //             { name: 'Legal', value: 4 },
+  //             
+
+  //             ]
+  //         },
+  //       ])
+  //         .then((answer) => {
+  //             DB.addEmployee(answer) 
+  //         //   console.log(DB.addEmployee(answer).first_name, answer.last_name,role_id,manager_id); 
+  //         }); 
+
+
+
+  // }
+
+
 }
+
+
+function createDept() {
+  inquirer
+    .prompt([
+      {
+        name: "department_name",
+        type: "input",
+        message: "What is the name of the department?",
+      },
+    ])
+    .then((answer) => {
+      let dept = answer.department_name;
+      DB.addDepartment(dept)
+      console.log(`added ${dept}`);
+      init()
+    })
+}
+
+function createEmployee() {
+
+  DB.viewAllRoles()
+    .then(([roles]) => {
+      const roleOptions = roles.map(({ id, title }) => ({
+        name: title,
+        value: id
+      }))
+  
+      DB.viewAllEmployees()
+      .then(([employees]) => {
+        const managerOptions = employees.map(({ id, first_name, last_name }) => ({
+          name: `${first_name} ${last_name}`,
+          value: id
+        }))
+
+  inquirer
+    .prompt([
+      {
+        name: "first_name",
+        type: "input",
+        message: "What is the employee's first name?",
+      },
+      {
+        name: "last_name",
+        type: "input",
+        message: "What is the employee's last name?",
+      },
+      {
+        name: "role_id",
+        type: "list",
+        message: "What is the employee's role?",
+        choices: roleOptions
+      },
+      {
+        name: "manager_id",
+        type: "list",
+        message: "Who is the employee's manager?",
+        choices: managerOptions
+      },
+
+    ])
+    .then((answer) => {
+
+      let newEmployee = {
+        first_name:answer.first_name,
+        last_name:answer.last_name,
+        role_id:answer.role_id,
+        manager_id:answer.manager_id,
+
+      }
+      DB.addEmployee(newEmployee);
+        console.log(`Added new employee: ${newEmployee}`); 
+        init();
+
+    })
+  })
+})
+}
+
 init()
